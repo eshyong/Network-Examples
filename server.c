d63fc50bccf80cbdfcc75ade67de71c794061149
eshyong <eric.shyong@gmail.com>
2013-09-02 19:27:25 -0700

    added files

---
+++ server.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "8000"
+
+#define BACKLOG 10
+
+void sigchld_handler(int s) {
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void) {
+	int sock_fd, new_fd;	// listen on sock_fd, new connection on fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr;	// connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can 
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sock_fd = socket(p->ai_family, p->ai_socktype,
+								p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes,
+							sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sock_fd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sock_fd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if (listen(sock_fd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	int connect = 1;
+	int status = 1;
+
+	while (connect) {	// main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sock_fd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		connect = 0;
+	}
+
+	char msg[1024];
+	int numbytes;
+
+	while (status) {
+		scanf("%s", msg);
+		if (strcmp(msg, "") > 0) {
+			numbytes = send(new_fd, msg, strlen(msg), 0);
+		}
+
+		if (strcmp(msg, "exit") == 0) {
+			close(sock_fd);
+			close(new_fd);
+			status = 0;
+		}
+	}
+
+	close(sock_fd);
+	close(new_fd);
+
+	return 0;
+}
